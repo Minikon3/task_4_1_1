dafb45df89f48fb32d32f57195ce43c99408c969
Minikon3 <112776259+Minikon3@users.noreply.github.com>
2022-10-10 18:06:14 +0300

    add

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+
 public class Main {
+    public static void printInfo(Seasons season){
+        System.out.print(season.name()+" --- "+season+" --- ");
+        season.getDescription();
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (Seasons season : Seasons.values()){
+            printInfo(season);
+        }
+        System.out.println();
+        System.out.print("Любимое время года: ");
+        printInfo(Seasons.valueOf("WINTER"));
     }
 }
--- src/Seasons.java
+++ src/Seasons.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Seasons {
+public enum Seasons {
+    SPRING(8),
+    SUMMER(22){
+        public void getDescription(){
+            System.out.println("Жаркое время года");
+        }
+    },
+    AUTUMN(15),
+    WINTER(-7);
+    private double averageTemp;
+    Seasons (double averageTemp){
+        this.averageTemp=averageTemp;
+    }
+
+    public double getAverageTemp() {
+        return averageTemp;
+    }
+
+    public void setAverageTemp(double averageTemp) {
+        this.averageTemp = averageTemp;
+    }
+    public void getDescription(){
+        System.out.println("Холодное время года");
+    }
+
+    @Override
+    public String toString() {
+        return "Средняя температура = "+averageTemp+" градусов";
+    }
 }
